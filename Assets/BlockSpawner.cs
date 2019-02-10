4ad594414edb7b5e18eebf83ea90e8be2009e578
DESKTOP-64L0P7U\peehu <ashish240284@gmail.com>
2019-02-10 13:39:06 +0530

    Initial commit

---
+++ Assets/BlockSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+
+    public Transform[] spawnpoints;
+    public GameObject blockPrefeb;
+    public float timeBetweenWaves = 1f;
+    private float timeToSpawn = 2f;
+
+
+
+    void Update() {
+
+        if (Time.time >= timeToSpawn) {
+
+            spawnBlock();
+
+            timeToSpawn = Time.time + timeBetweenWaves;
+
+        }
+    }
+
+   
+
+    void spawnBlock()
+    {
+        int randomIndex = Random.Range(0, spawnpoints.Length);
+
+        for (int i = 0; i < spawnpoints.Length; i++)
+        {
+            if (randomIndex != i) {
+
+                Instantiate(blockPrefeb, spawnpoints[i].position, Quaternion.identity);
+
+            }
+        }
+    }
+
+    
+}
---
+++ Assets/destroy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().gravityScale += Time.timeSinceLevelLoad / 20f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -4f) {
+            Destroy(gameObject);
+
+        }
+    }
+}
